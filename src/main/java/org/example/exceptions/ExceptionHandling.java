8f68e3f2be62ed29dd8034ada2e24f2d3329ad4c
szczynk <34181070+szczynk@users.noreply.github.com>
2023-08-02 00:43:03 +0700

    feat: checked and unchecked exception

--- src/main/java/org/example/exceptions/ExceptionHandling.java
+++ src/main/java/org/example/exceptions/ExceptionHandling.java
@@ -14,6 +14,48 @@ import java.io.IOException;
 // In a `catch` block, you can obtain more details about the exception
 // using methods like `getMessage()` to get the error message.
 //
+// e.g. on `ArrayIndexOutOfBoundsException`
+// ```java
+// String[] places = {“work”, “school”, “the cinema”};
+// for(int i=0; i<4; i++){
+//     System.out.println(“I am going to “ + places[i]);
+// }
+// ```
+//
+// ==============================
+//
+// Exceptions in Java are divided into two categories:
+// - checked    : - exceptions that are "checked" by the compiler.
+//                - The compiler ensures that calling methods "must handle"
+//                  these exceptions.
+//                - are used when it's "reasonable" that a calling method
+//                  can recover from the error.
+//                - e.g. on creating a new file
+//                  ```java
+//                  File file = new File("resources/nonexistent.txt");
+//
+//                  file.createNewFile();
+//                  // we can't compile this directly, we must put a try-catch
+//                  // because `createNewFile()` throwing an exception
+//                  ```
+//
+// - unchecked  : - exceptions that are "not checked" by the compiler.
+//                - are runtime exceptions, and handling these exceptions
+//                  is "optional".
+//                - are used when "there is no practical way to recover"
+//                  from the error.
+//                - While it's "optional", they can be caught and handled
+//                  using a try-catch block if needed.
+//                - e.g. on `InputMismatchException`
+//                  ```java
+//                  Scanner console = new Scanner(System.in);
+//
+//                  double input = console.nextDouble();
+//                  // this can compile, we don't have put a try-catch
+//                  // because `nextDouble()` didn't tell us it
+//                  // throwing an exception
+//                  ```
+//
 //
 // problem:
 // Create File
@@ -25,7 +67,7 @@ public class ExceptionHandling {
         try {
             file.createNewFile();
         } catch (IOException e) {
-            System.out.println("Sorry, an error occured: " + e.getMessage());
+            System.out.println("Sorry, an error occurred: " + e.getMessage());
         }
     }
 }
