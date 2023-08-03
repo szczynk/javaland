e0062c7c372a0919d53a0ce28aa6433ee37024d0
szczynk <34181070+szczynk@users.noreply.github.com>
2023-08-03 21:26:00 +0700

    Create EmployeeServiceImpl.java

---
+++ src/main/java/org/example/payroll/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package org.example.payroll.service;
+
+import org.example.payroll.model.Employee;
+import org.example.payroll.model.Salary;
+import org.example.payroll.repository.EmployeeRepo;
+import org.example.payroll.repository.SalaryRepo;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    private final SalaryRepo salaryRepo;
+
+    public EmployeeServiceImpl(EmployeeRepo employeeRepo, SalaryRepo salaryRepo) {
+        this.employeeRepo = employeeRepo;
+        this.salaryRepo = salaryRepo;
+    }
+
+    @Override
+    public List<Employee> list() {
+        return employeeRepo.list();
+    }
+
+    @Override
+    public int add(Employee employee) {
+        var salaries = salaryRepo.list();
+
+        boolean isValidGrade = false;
+        for (Salary salary : salaries) {
+            if (employee.getGrade() == salary.getGrade()) {
+                isValidGrade = true;
+                break;
+            }
+        }
+        if (!isValidGrade) {
+            return -1;
+        }
+
+        return employeeRepo.add(employee);
+    }
+
+    @Override
+    public Employee detail(int id) {
+        return employeeRepo.detail(id);
+    }
+}
