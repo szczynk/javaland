77c6d94711c7e38869f7ab87e53a45c21e5e44e8
szczynk <34181070+szczynk@users.noreply.github.com>
2023-08-03 21:27:19 +0700

    Create Main.java

---
+++ src/main/java/org/example/payroll/Main.java
@@ -0,0 +1,31 @@
+package org.example.payroll;
+
+import org.example.payroll.repository.*;
+import org.example.payroll.service.*;
+import org.example.payroll.handler.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeRepo employeeRepo = new EmployeeInMemoryRepoImpl();
+        SalaryRepo salaryRepo = new SalaryInMemoryRepoImpl();
+        PayrollRepo payrollRepo = new PayrollInMemoryRepoImpl();
+
+
+        EmployeeService employeeService = new EmployeeServiceImpl(employeeRepo, salaryRepo);
+        SalaryService salaryService = new SalaryServiceImpl(salaryRepo);
+        PayrollService payrollService = new PayrollServiceImpl(payrollRepo, employeeRepo, salaryRepo);
+
+
+        Scanner scanner = new Scanner(System.in);
+        EmployeeHandler employeeHandler = new EmployeeCLIHandlerImpl(scanner,employeeService);
+        SalaryHandler salaryHandler = new SalaryCLIHandlerImpl(salaryService);
+        PayrollHandler payrollHandler = new PayrollCLIHandlerImpl(scanner,payrollService);
+
+
+        Menu menu = new Menu(scanner,employeeHandler, salaryHandler, payrollHandler);
+        menu.show();
+    }
+}
+
