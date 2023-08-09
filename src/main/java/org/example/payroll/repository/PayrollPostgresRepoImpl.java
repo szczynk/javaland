af760992bda0fe6d7b9712f0a3a4b39162875d56
szczynk <34181070+szczynk@users.noreply.github.com>
2023-08-09 17:38:08 +0700

    Update PayrollPostgresRepoImpl.java

--- src/main/java/org/example/payroll/repository/PayrollPostgresRepoImpl.java
+++ src/main/java/org/example/payroll/repository/PayrollPostgresRepoImpl.java
@@ -5,14 +5,11 @@ import org.example.payroll.model.Employee;
 import org.example.payroll.model.Payroll;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PayrollPostgresRepoImpl implements PayrollRepo{
+public class PayrollPostgresRepoImpl implements PayrollRepo {
     private final DataSource dataSource;
 
     public PayrollPostgresRepoImpl(DataSource dataSource) {
@@ -29,9 +26,9 @@ public class PayrollPostgresRepoImpl implements PayrollRepo{
 
     @Override
     public List<Payroll> list() {
-        String sqlQuery = "SELECT id, name, gender, grade, married FROM employees ORDER BY id ASC";
+        String sqlQuery = "SELECT payrolls.id, payrolls.basic_salary, payrolls.pay_cut, payrolls.additional_salary, payrolls.employee_id, employees.id, employees.name, employees.gender, employees.grade, employees.married FROM payrolls INNER JOIN employees ON payrolls.employee_id = employees.id ORDER BY payrolls.id ASC";
 
-        List<Employee> employees = new ArrayList<>();
+        List<Payroll> payrolls = new ArrayList<>();
 
         try (
                 Connection connection = dataSource.getConnection();
@@ -39,29 +36,86 @@ public class PayrollPostgresRepoImpl implements PayrollRepo{
                 ResultSet resultSet = statement.executeQuery(sqlQuery);
         ) {
             while (resultSet.next()) {
-                Employee employee = new Employee();
-                employee.setId(resultSet.getInt("id"));
-                employee.setName(resultSet.getString("name"));
-                employee.setGender(resultSet.getString("gender"));
-                employee.setGrade(resultSet.getInt("grade"));
-                employee.setMarried(resultSet.getBoolean("married"));
+                Payroll payroll = new Payroll(
+                        resultSet.getInt("id"),
+                        resultSet.getLong("basic_salary"),
+                        resultSet.getLong("pay_cut"),
+                        resultSet.getLong("additional_salary"),
+                        new Employee(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("gender"),
+                                resultSet.getInt("grade"),
+                                resultSet.getBoolean("married")
+                        ),
+                        resultSet.getInt("employee_id")
+                );
 
-                employees.add(employee);
+                payrolls.add(payroll);
             }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
 
-        return employees;
+        return payrolls;
     }
 
     @Override
     public int add(Payroll payroll) {
+        String sqlQuery = "INSERT INTO payrolls (basic_salary, pay_cut, additional_salary, employee_id) VALUES (?, ?, ?, ?) RETURNING id";
+
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        ) {
+            statement.setLong(1, payroll.getBasicSalary());
+            statement.setLong(2, payroll.getPayCut());
+            statement.setLong(3, payroll.getAdditionalSalary());
+            statement.setInt(4, payroll.getEmployeeId());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
         return 0;
     }
 
     @Override
     public Payroll detail(int id) {
+        String sqlQuery = "SELECT payrolls.id, payrolls.basic_salary, payrolls.pay_cut, payrolls.additional_salary, payrolls.employee_id, employees.id, employees.name, employees.gender, employees.grade, employees.married FROM payrolls INNER JOIN employees ON payrolls.employee_id = employees.id WHERE payrolls.id = ? LIMIT 1";
+
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sqlQuery);
+        ) {
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return new Payroll(
+                        resultSet.getInt("id"),
+                        resultSet.getLong("basic_salary"),
+                        resultSet.getLong("pay_cut"),
+                        resultSet.getLong("additional_salary"),
+                        new Employee(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("gender"),
+                                resultSet.getInt("grade"),
+                                resultSet.getBoolean("married")
+                        ),
+                        resultSet.getInt("employee_id")
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
         return null;
     }
 }
